dc2c9723b38dcd15215c3d53dc2c1393e1bc5398
Sebastian Beck <sebas.b@web.de>
2018-08-29 14:30:14 +0200

    removed unnecessary classes

--- src/main/java/dlaudio/Application.java
+++ src/main/java/dlaudio/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
-@EnableConfigurationProperties(StorageProperties.class)
+//@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 	
     public static void main(String[] args) {
