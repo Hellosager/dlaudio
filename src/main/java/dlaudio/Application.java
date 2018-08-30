878b30ac5be86dada018ed9f7ea203a1abdb30e5
Sebastian Beck <sebas.b@web.de>
2018-08-30 18:30:43 +0200

    stable prototype solution, made root to landingpage, both request types functioning

--- src/main/java/dlaudio/Application.java
+++ src/main/java/dlaudio/Application.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
-//@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
 	
     public static void main(String[] args) {
--- src/main/java/dlaudio/DownloadController.java
+++ src/main/java/dlaudio/DownloadController.java
@@ -3,22 +3,25 @@ package dlaudio;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import AudioStripper.AudioStripper;
 
@@ -28,21 +31,18 @@ import AudioStripper.AudioStripper;
 public class DownloadController {
 	
 	@RequestMapping(value="/convert", method=RequestMethod.GET)
-	public void getMP3FromVideo(@RequestParam("link") String link, HttpServletResponse response) throws IOException, InterruptedException {
-		System.out.println("VIDEO DOWNLOAD NICE");
+	public RedirectView getMP3FromVideo(@RequestParam("link") String link, RedirectAttributes attributes) throws IOException, InterruptedException {
 		AudioStripper as = new AudioStripper("Videos", "Musik");
-		String pathMp3File = as.stripAudioFromVideoLink(link).replaceAll(".mp4", ".mp3");
+		String pathMp3File = as.stripAudioFromVideoLink(link).replaceAll(".mp4", ".mp3").replaceFirst("Musik\\\\", "");
 		System.out.println("resName: " + pathMp3File);
-//		response.setHeader("Content-Disposition", "attachment;filename=\"" + pathMp3File + "\"");
-//		InputStream stream = new FileInputStream(new File(pathMp3File));
-//		IOUtils.copy(stream, response.getOutputStream());
 		
-		System.out.println("----------------WIEDER FREI----------------");
+		attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+		attributes.addAttribute("attribute", "redirectWithRedirectView");
+		return new RedirectView("download/" + pathMp3File);
 	}
 	
 	@RequestMapping(value="/download/{resourceName}", method=RequestMethod.GET)
 	public void downloadResource(@PathVariable(value="resourceName") String resourceName, HttpServletResponse response) {
-		System.out.println("drin");
 		response.setHeader("Content-Disposition", "attachment;filename=\"" + "Musik\\" + resourceName + "\"");	// TODO HARDCODED
 		InputStream stream;
 		try {
@@ -58,14 +58,43 @@ public class DownloadController {
 	}
 
 	@RequestMapping(value="/convert", method=RequestMethod.POST)
-	public String getMP3sFromVideo(@ModelAttribute("textFile")MultipartFile textFile, ModelMap model) throws IOException, InterruptedException {
+	public RedirectView getMP3sFromVideo(@ModelAttribute("textFile")MultipartFile textFile, RedirectAttributes attributes) throws IOException, InterruptedException {
 		System.out.println("inmethod");
+		attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+		attributes.addAttribute("attribute", "redirectWithRedirectView");
 		String fileContent = new String(textFile.getBytes());
 		String[] links = fileContent.split("\n");
 		AudioStripper as = new AudioStripper("Videos", "Musik");
-		as.stripAudioFromVideoLinks(links);
+		String[] downloadedMP3Paths = as.stripAudioFromVideoLinks(links);
 
-		return "download";
+		// Zip it
+		String zipName = zipFiles(downloadedMP3Paths);
+		
+		return new RedirectView("download/" + zipName);
 	}
 	
+	
+	private String zipFiles(String[] filePaths) throws IOException {
+		String uniqueZipName = "converted-mp3s-" + System.currentTimeMillis() + ".zip";
+		System.out.println("Storing downloaded files in " + "Musik\\" + uniqueZipName);
+		FileOutputStream fos = new FileOutputStream("Musik\\" + uniqueZipName);
+		ZipOutputStream zipOutputStream = new ZipOutputStream(fos);
+		for(String path : filePaths) {
+			File fileToZip = new File(path);
+			FileInputStream fis = new FileInputStream(fileToZip);
+			ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
+			zipOutputStream.putNextEntry(zipEntry);
+			
+			byte[] bytes = new byte[1024];
+			int length;
+			while((length = fis.read(bytes)) >= 0) {
+				zipOutputStream.write(bytes, 0, length);
+			}
+			fis.close();
+		}
+		zipOutputStream.close();
+		fos.close();
+		
+		return uniqueZipName;
+	}
 }
