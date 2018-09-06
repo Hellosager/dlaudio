a88510a55d2eb59f8383c0c8e48633791f98d9e3
Sebastian Beck <sebas.b@web.de>
2018-09-06 22:46:27 +0200

    implemented basic prototype of progress bar

--- src/main/java/dlaudio/DownloadController.java
+++ src/main/java/dlaudio/DownloadController.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -13,40 +15,55 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import AudioStripper.AudioStripper;
 
-
-
 @Controller
 public class DownloadController {
-	
-	@RequestMapping(value="/convert", method=RequestMethod.GET)
-	public RedirectView getMP3FromVideo(@RequestParam("link") String link, RedirectAttributes attributes) throws IOException, InterruptedException {
+
+	@RequestMapping(value = "/convert", method = RequestMethod.GET)
+	public String getMP3FromVideo(@RequestParam("link") String link, ModelAndView model)
+			throws IOException, InterruptedException {
 		AudioStripper as = new AudioStripper("Videos", "Musik");
-		String pathMp3File = as.stripAudioFromVideoLink(link).replaceAll(".mp4", ".mp3").replaceFirst("Musik\\\\", "");
-		System.out.println("resName: " + pathMp3File);
-		
-		attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
-		attributes.addAttribute("attribute", "redirectWithRedirectView");
-		return new RedirectView("download/" + pathMp3File);
+		new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					String pathMp3File = as.stripAudioFromVideoLink(link).replaceAll(".mp4", ".mp3")
+							.replaceFirst("Musik\\\\", "");
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+		System.out.println("FREE");
+		// System.out.println("resName: " + pathMp3File);
+
+		System.out.println("redirecting...");
+		return "progress";
 	}
-	
-	@RequestMapping(value="/download/{resourceName}", method=RequestMethod.GET)
-	public void downloadResource(@PathVariable(value="resourceName") String resourceName, HttpServletResponse response) {
-		response.setHeader("Content-Disposition", "attachment;filename=\"" + resourceName + "\"");	// TODO HARDCODED
+
+	@RequestMapping(value = "/download/{resourceName}", method = RequestMethod.GET)
+	public void downloadResource(@PathVariable(value = "resourceName") String resourceName,
+			HttpServletResponse response) {
+		response.setHeader("Content-Disposition", "attachment;filename=\"" + resourceName + "\""); // TODO HARDCODED
 		InputStream stream;
 		try {
-			stream = new FileInputStream(new File("Musik\\" + resourceName));	// TODO HARDCODED
+			stream = new FileInputStream(new File("Musik\\" + resourceName)); // TODO HARDCODED
 			IOUtils.copy(stream, response.getOutputStream());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -57,8 +74,9 @@ public class DownloadController {
 		}
 	}
 
-	@RequestMapping(value="/convert", method=RequestMethod.POST)
-	public RedirectView getMP3sFromVideo(@ModelAttribute("textFile")MultipartFile textFile, RedirectAttributes attributes) throws IOException, InterruptedException {
+	@RequestMapping(value = "/convert", method = RequestMethod.POST)
+	public RedirectView getMP3sFromVideo(@ModelAttribute("textFile") MultipartFile textFile,
+			RedirectAttributes attributes) throws IOException, InterruptedException {
 		System.out.println("inmethod");
 		attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
 		attributes.addAttribute("attribute", "redirectWithRedirectView");
@@ -69,32 +87,50 @@ public class DownloadController {
 
 		// Zip it
 		String zipName = zipFiles(downloadedMP3Paths);
-		
+
 		return new RedirectView("download/" + zipName);
 	}
-	
-	
+
+	// TODO find way to get duration from video with ffmpeg output
+	// TODO remove hardcode and replace it with logic that calculates processed video
+	// from video duration
+	private int progress = 0;
+
+	@GetMapping(value = "/retrieveProgress", produces = "application/json")
+	@ResponseBody
+	public Map<String, Integer> retrieveProgress() {
+		progress++;
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("progress", progress);
+		return map;
+	}
+
+	@RequestMapping("/progress")
+	public String progress() {
+		return "progress";
+	}
+
 	private String zipFiles(String[] filePaths) throws IOException {
 		String uniqueZipName = "converted-mp3s-" + System.currentTimeMillis() + ".zip";
 		System.out.println("Storing downloaded files in " + "Musik\\" + uniqueZipName);
 		FileOutputStream fos = new FileOutputStream("Musik\\" + uniqueZipName);
 		ZipOutputStream zipOutputStream = new ZipOutputStream(fos);
-		for(String path : filePaths) {
+		for (String path : filePaths) {
 			File fileToZip = new File(path);
 			FileInputStream fis = new FileInputStream(fileToZip);
 			ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
 			zipOutputStream.putNextEntry(zipEntry);
-			
+
 			byte[] bytes = new byte[1024];
 			int length;
-			while((length = fis.read(bytes)) >= 0) {
+			while ((length = fis.read(bytes)) >= 0) {
 				zipOutputStream.write(bytes, 0, length);
 			}
 			fis.close();
 		}
 		zipOutputStream.close();
 		fos.close();
-		
+
 		return uniqueZipName;
 	}
 }
