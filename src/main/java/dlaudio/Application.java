60249aad57d824f4eb7fe580b79eb37857e77a70
Sebastian Beck <sebas.b@web.de>
2018-09-18 22:31:54 +0200

    organized imports

--- src/main/java/dlaudio/Application.java
+++ src/main/java/dlaudio/Application.java
@@ -11,8 +11,6 @@ import java.util.jar.JarFile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Application {
